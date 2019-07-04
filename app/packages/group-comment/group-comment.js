389c67aee9e0e35953f6d6bcba365672b379c548
bigsea <iwangyang@qq.com>
2019-07-04 14:12:52 +0800

    group

--- app/packages/group-comment/group-comment.js
+++ app/packages/group-comment/group-comment.js
@@ -184,33 +184,13 @@ Component({
       },
     ],
   },
-  attached() {
-    wx.showModal({
-      title: '尊敬的产品，请选择您要测试的账号：',
-      content: '露姐（Admin）/ 大海（User）',
-      cancelText: '露姐',
-      cancelColor: '#E88536',
-      confirmText: '大海',
-      confirmColor: '#448ACA',
-      success: (res) => {
-        if (res.confirm) {
-          this.setData({
-            userInfo: {
-              name: '大海',
-              head:
-                'http://statics03.qingmang.mobi/image/proxy/aHR0cHMlM0EvL3d4LnFsb2dvLmNuL21tb3Blbi92aV8zMi9EWUFJT2dxODNlcEd4Y29SaWI5aWE3SGdXNWN3akdEbEZrdDhUVExxRmxKaHJtMm5oTmJaQUpkdUMzMlJpYjV1M29RTHBHWlEzSXMyQ09weFh3aWNjUmdNNEEvMTMy',
-            },
-          })
-        }
-      },
-    })
-  },
+  attached() {},
   methods: {
     bindImg(event) {
       let data = event.currentTarget.dataset
       wx.previewImage({
         current: data.src, // 当前显示图片的http链接
-        urls: data.imgs // 需要预览的图片http链接列表
+        urls: data.imgs, // 需要预览的图片http链接列表
       })
     },
     bindMore(event) {
