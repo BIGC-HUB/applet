b5a54a6f6237198b236147679dc157049b69945d
bigsea <iwangyang@qq.com>
2019-07-06 11:10:08 +0800

    toComment

--- app/pages/group/group.js
+++ app/pages/group/group.js
@@ -1,24 +1,8 @@
+const util = require('../../packages/bigsea.js')
 Page({
   data: {},
-  onLoad() {
-    // wx.showModal({
-    //   title: '尊敬的产品，请选择您要测试的账号：',
-    //   content: '露姐（Admin）/ 大海（User）',
-    //   cancelText: '露姐',
-    //   cancelColor: '#E88536',
-    //   confirmText: '大海',
-    //   confirmColor: '#448ACA',
-    //   success: (res) => {
-    //     if (res.confirm) {
-    //       this.setData({
-    //         userInfo: {
-    //           name: '大海',
-    //           head:
-    //             'http://statics03.qingmang.mobi/image/proxy/aHR0cHMlM0EvL3d4LnFsb2dvLmNuL21tb3Blbi92aV8zMi9EWUFJT2dxODNlcEd4Y29SaWI5aWE3SGdXNWN3akdEbEZrdDhUVExxRmxKaHJtMm5oTmJaQUpkdUMzMlJpYjV1M29RTHBHWlEzSXMyQ09weFh3aWNjUmdNNEEvMTMy',
-    //         },
-    //       })
-    //     }
-    //   },
-    // })
-  }
+  onLoad() {},
+  bindToComment() {
+    util.path('pages/group/groupComment')
+  },
 })
